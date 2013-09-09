49271720bca25df9cb0edaa77ecbf586c31e389f
Vitaly Serov <serovvitaly@gmail.com>
2013-09-10 03:55:50 +0600

    добавлены основные вьюхи

---
+++ app/views/home/all.blade.php
@@ -0,0 +1,39 @@
+<table class="table table-condensed table-bordered table-striped table-hover">
+  <thead>
+    <tr>
+      <th>Домен</th>
+      <th>Страницы</th>
+      <th>Делигирован</th>
+      <th>Статус</th>
+      <th>Телефон1</th>
+      <th>Телефон2</th>
+      <th>Email</th>
+      <th>Дата п. обр</th>
+      <th>В статусе</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>foobar.ru</td>
+      <td>3</td>
+      <td>да</td>
+      <td>есть контакты</td>
+      <td>89234567</td>
+      <td>89234567</td>
+      <td>foo@bar.ru</td>
+      <td>23.08.2013</td>
+      <td>23.08.2013</td>
+    </tr>
+    <tr>
+      <td>foobar.ru</td>
+      <td>3</td>
+      <td>да</td>
+      <td>есть контакты</td>
+      <td>89234567</td>
+      <td>89234567</td>
+      <td>foo@bar.ru</td>
+      <td>23.08.2013</td>
+      <td>23.08.2013</td>
+    </tr>
+  </tbody>
+</table>
