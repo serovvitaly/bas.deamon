e0a37a3a27ef62c1c50dbb6aec24bd9dfca9208c
Vitaly Serov <serovvitaly@gmail.com>
2013-09-10 15:54:22 +0600

    страница загрузки файлов

--- app/views/home/load.blade.php
+++ app/views/home/load.blade.php
@@ -1,1 +1,16 @@
-LOAD
+<button class="btn">Загрузка</button>
+
+<fieldset>
+  <legend>Статистика</legend>
+  
+  <table class="table table-bordered table-striped">
+    <thead><tr>
+      <th>Имя файла</th>
+      <th>Дата загрузки</th>
+      <th>Объем файла</th>
+      <th>Найдено ссылок</th>
+      <th>Добавлено в базу</th>
+    </tr></thead>
+  </table>
+  
+</fieldset>
