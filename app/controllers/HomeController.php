f41fa14a124308b149f3e62bb98da9954cbf8cd5
Vitaly Serov <serovvitaly@gmail.com>
2013-09-17 16:13:58 +0600

    логирование

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -199,10 +199,11 @@ class HomeController extends BaseController {
     protected function _process_go($file_path, $file_id)
     {   
         $log = $_SERVER['DOCUMENT_ROOT'] . '/data.log';
+        echo $log;
         $log_text = 'Первый процесс';
         file_put_contents($log, date('d.m.Y H:i:s') . ' - ' . $log_text . PHP_EOL, FILE_APPEND);
     
-        echo $log; 
+        
         /*$child_pid = pcntl_fork();
         if ($child_pid) {
             exit();
