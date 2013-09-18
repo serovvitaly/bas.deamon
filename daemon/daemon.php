3fbeae0a5c863eada8e4f8089e6b8b3cdb34d816
Vitaly Serov <serovvitaly@gmail.com>
2013-09-19 05:15:02 +0600

    работа над демоном

--- daemon/daemon.php
+++ daemon/daemon.php
@@ -1,22 +1,22 @@
 <?php
-/*
+
 $child_pid = pcntl_fork();
 if ($child_pid) {
     exit();
 }
 posix_setsid();
-*/
-$baseDir = '/var/www/tester/data/spider';
 
-//ini_set('error_log', $baseDir . '/logs/daemon_error.log');
+$baseDir = dirname(__FILE__);
 
-//fclose(STDIN);
-//fclose(STDOUT);
-//fclose(STDERR);
+ini_set('error_log', $baseDir . '/logs/daemon_error.log');
 
-//$STDIN  = fopen('/dev/null', 'r');
-//$STDOUT = fopen($baseDir.'/logs/application.log', 'ab');
-//$STDERR = fopen($baseDir.'/logs/daemon.log', 'ab');
+fclose(STDIN);
+fclose(STDOUT);
+fclose(STDERR);
+
+$STDIN  = fopen('/dev/null', 'r');
+$STDOUT = fopen($baseDir.'/logs/daemon.log', 'ab');
+$STDERR = fopen($baseDir.'/logs/daemon.log', 'ab');
 
 include 'include/Daemon.class.php';
 $daemon = new Daemon();
