9b7c96f98ef73483fe2cf867ccb0057289dbf8c9
Vitaly Serov <serovvitaly@gmail.com>
2013-09-20 01:42:21 +0600

    фон проц

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -102,7 +102,7 @@ class HomeController extends BaseController {
         array_walk($mix, function(&$item, $key){
             $item = explode(' ', $item);
             array_walk($item, function(&$item, $key){
-                if (empty($item)) $item = 'EMPTY';
+                if (empty($item)) unset($item);
             });
         });
         
