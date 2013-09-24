4adbb80e5c27dec8b8992cfdacfe60f3762859b4
Vitaly Serov <serovvitaly@gmail.com>
2013-09-25 00:29:54 +0600

    интерфейс

--- app/views/common/grid.blade.php
+++ app/views/common/grid.blade.php
@@ -35,8 +35,8 @@
       <td><?= $item->internal_links_count ?></td>
       <td><?= $delegated[$item->delegated] ?></td>
       <td><?= $statuses[$item->status] ?></td>
-      <td style="text-align: center;"><?= empty($item->phones) ? '' : '<img src="/packages/icons/tick_6817.png" alt="">' ?></td>
-      <td style="text-align: center;"><?= empty($item->emails) ? '' : '<img src="/packages/icons/tick_6817.png" alt="">' ?></td>
+      <td style="text-align: center;"><?= !empty($item->phones) ? '' : '<img class="popover" data-toggle="popover" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus." data-placement="right" src="/packages/icons/tick_6817.png" alt="">' ?></td>
+      <td style="text-align: center;"><?= empty($item->emails) ? '' : '<img class="popover" data-toggle="popover" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus." data-placement="right" src="/packages/icons/tick_6817.png" alt="">' ?></td>
       <td><?= $item->updated_at ?></td>
       <td><?= $item->updated_at ?></td>
     </tr>
@@ -54,3 +54,7 @@
 </table>
 
 <?php echo $items->links(); ?>
+
+<script>
+$('.popover').popover();
+</script>
