d0e49329deb344635b57ec1cdfaaf21326fc8857
Vitaly Serov <serovvitaly@gmail.com>
2013-09-24 23:39:55 +0600

    интерфейс

--- app/views/common/grid.blade.php
+++ app/views/common/grid.blade.php
@@ -35,8 +35,8 @@
       <td><?= $item->internal_links_count ?></td>
       <td><?= $delegated[$item->delegated] ?></td>
       <td><?= $statuses[$item->status] ?></td>
-      <td style="text-align: center;"><?= empty($item->phones) ? '' : '<img src="/icons/tick_6817.png" alt="">' ?></td>
-      <td style="text-align: center;"><?= empty($item->emails) ? '' : '<img src="/icons/tick_6817.png" alt="">' ?></td>
+      <td style="text-align: center;"><?= empty($item->phones) ? '' : '<img src="/packages/icons/tick_6817.png" alt="">' ?></td>
+      <td style="text-align: center;"><?= empty($item->emails) ? '' : '<img src="/packages/icons/tick_6817.png" alt="">' ?></td>
       <td><?= $item->updated_at ?></td>
       <td><?= $item->updated_at ?></td>
     </tr>
