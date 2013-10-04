be1fb7f220c717562e03477ceb5a600971d6d653
Vitaly Serov <serovvitaly@gmail.com>
2013-10-04 10:54:05 +0600

    очередной коммит

--- app/views/home/proven.blade.php
+++ app/views/home/proven.blade.php
@@ -116,7 +116,7 @@ function goPage(page, data_key){
                         console.log(index, item);
                         $('#markers-content').append('<a href="#" onclick="setMarker(\''+index+'\'); return false;">'+index+' ('+item+')</a>');
                     });
-                    console.log('markers', markers);
+                    console.log('markers', markers[0]);
                     
                     _unload();
                     $('#export-button').attr('href', '/export?date='+slid[1]);
