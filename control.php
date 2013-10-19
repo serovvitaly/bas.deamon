857c00ef8874b63a0a2fddc3fa39b425eed3e876
Vitaly Serov <serovvitaly@gmail.com>
2013-10-19 19:51:24 +0600

    основной контроллер

---
+++ control.php
@@ -0,0 +1,8 @@
+<?php
+
+$token = (isset($_REQUEST['token']) AND !empty($_REQUEST['token'])) ? $_REQUEST['token'] : NULL;
+
+if ($token == NULL OR $token != '3246346257yterrtt3466rehjg45hdhkjg46557uvgmgtt4t56ygh67u46') {
+    exit();
+}
+
