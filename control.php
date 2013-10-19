e31150cecb0ccc9db7b60b6e296fd27bbcc3b0fb
Vitaly Serov <serovvitaly@gmail.com>
2013-10-19 19:58:54 +0600

    очередной коммит

--- control.php
+++ control.php
@@ -6,3 +6,18 @@ if ($token == NULL OR $token != '3246346257yterrtt3466rehjg45hdhkjg46557uvgmgtt4
     exit();
 }
 
+$action = (isset($_REQUEST['action']) AND !empty($_REQUEST['action'])) ? $_REQUEST['action'] : NULL;
+
+switch ($action) {
+    case 'run':
+        
+        $root_path = dirname(__FILE__);
+        $daemon_path = $root_path . '/daemon/daemon.php';
+        $daemon_log_path = $root_path . '/daemon/logs/daemon.log';
+        
+        $command = "/usr/bin/php -f {$daemon_path} > {$daemon_log_path} &";
+        
+        exec($command);
+        
+        break;
+}
