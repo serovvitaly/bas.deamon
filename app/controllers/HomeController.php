7063db050b179e7d2452a65a60f03e52dd09d6fa
Vitaly Serov <serovvitaly@gmail.com>
2013-10-20 11:22:19 +0600

    очередной коммит

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -154,7 +154,7 @@ class HomeController extends BaseController {
         $sites = Site::where('status', '>=', $_satus)->where('status', '<', 4)->whereIn('status', $filter_status)->orderBy('domain_created', 'DESC')->paginate(50);
         
         foreach ($filter_status AS $filter_stat) {
-            $sites->appends('filter_status[]', $filter_stat);
+            $sites->appends('filter_status['.$filter_stat.']', $filter_stat);
         }
         
         if ($uid > 0 AND ($dm = Site::find($uid))) {
