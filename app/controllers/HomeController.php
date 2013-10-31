a9835213f09d2dbb4041014cc9950fad4d2a4596
Vitaly Serov <serovvitaly@gmail.com>
2013-10-31 22:38:59 +0600

    очередной коммит

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -144,6 +144,8 @@ class HomeController extends BaseController {
             
             switch ($type) {
                 case 'phone':
+                    $ptn = Pattern::get('pattern');
+                    var_dump($ptn);
                     $pattern = '/((\\\\((8|7|\+7|\+\s7){0,1}(9){1}[0-9]{1}\\\\)[\s]{0,})|((8|7|\+7|\+\s7){0,1}[\s]{0,}[- \\\\(]{0,}([0-9]{3,4})[- \\\\)]{0,}))[0-9]{1,3}(-){0,}[0-9]{2}(-)[0-9]{2}/';
                     break;
                 case 'email':
@@ -158,10 +160,10 @@ class HomeController extends BaseController {
                 
                 if ($content) {
                     
-                    preg_match_all($pattern, $content, $matches1);
+                    @preg_match_all($pattern, $content, $matches1);
                     
                     $content = strip_tags($content);
-                    preg_match_all($pattern, $content, $matches2);
+                    @preg_match_all($pattern, $content, $matches2);
                     
                     //$out['data'] = array_merge_recursive($matches1, $matches2);
                     
