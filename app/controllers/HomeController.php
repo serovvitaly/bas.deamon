57844630aef3bc2f2e4dd1f58aac8966e17de220
Vitaly Serov <serovvitaly@gmail.com>
2013-11-01 02:55:53 +0600

    очередной коммит

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -210,7 +210,18 @@ class HomeController extends BaseController {
             $out['pattern2'] = $pattern;
             
             if ($pattern !== NULL) {
-                $content = file_get_contents($url);
+                
+                $options = array(
+                  'http'=>array(
+                    'method'=>"GET",
+                    'header'=>"Accept-language: en\r\n" .
+                              "Cookie: foo=bar\r\n" .  // check function.stream-context-create on php.net
+                              "User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n" // i.e. An iPad 
+                  )
+                );
+
+                $context = stream_context_create($options);
+                $content = file_get_contents($url, false, $context);
                 
                 if ($content) {
                     
