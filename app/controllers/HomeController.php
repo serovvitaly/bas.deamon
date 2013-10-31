8070fbb0d4c3b05e736d789dbf48185a004bc46f
Vitaly Serov <serovvitaly@gmail.com>
2013-10-31 22:40:11 +0600

    очередной коммит

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -144,7 +144,7 @@ class HomeController extends BaseController {
             
             switch ($type) {
                 case 'phone':
-                    $ptn = Pattern::get('pattern');
+                    $ptn = Pattern::all();
                     var_dump($ptn);
                     $pattern = '/((\\\\((8|7|\+7|\+\s7){0,1}(9){1}[0-9]{1}\\\\)[\s]{0,})|((8|7|\+7|\+\s7){0,1}[\s]{0,}[- \\\\(]{0,}([0-9]{3,4})[- \\\\)]{0,}))[0-9]{1,3}(-){0,}[0-9]{2}(-)[0-9]{2}/';
                     break;
