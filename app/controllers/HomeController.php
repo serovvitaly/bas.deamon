0e95f734ca44f6bcc79ecb8e18dc67688bedddd0
Vitaly Serov <serovvitaly@gmail.com>
2013-11-01 21:25:42 +0600

    тест проверки контактов

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -230,7 +230,7 @@ class HomeController extends BaseController {
                     $content = strip_tags($content);
                     preg_match_all($pattern, $content, $matches2);
                     
-                    //$out['data'] = array_merge_recursive($matches1, $matches2);
+                    $out['data'] = array_merge_recursive($matches1, $matches2);
                     
                     $result = array();
                     
