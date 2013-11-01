7d8be10048b67ad484dbd5bbf0be759ccfc21b18
Vitaly Serov <serovvitaly@gmail.com>
2013-11-01 23:23:49 +0600

    очередной коммит

--- app/views/home/checker.blade.php
+++ app/views/home/checker.blade.php
@@ -122,7 +122,7 @@
     </form>
 
     <div>
-      <iframe src="/get-url-content?url={{ $url }}" id="load-container" style="width: 100%; height: 600px; border: 1px solid #CECECE;" onload="frameLoaded()"></iframe>
+      <iframe sandbox src="{{ $url }}" id="load-container" style="width: 100%; height: 600px; border: 1px solid #CECECE;" onload="frameLoaded()"></iframe>
     </div> 
     
   </div>
