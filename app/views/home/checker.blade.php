9feb8e1a9a8c298c0ac95c7c9b0bfeade8012db2
Vitaly Serov <serovvitaly@gmail.com>
2013-11-01 23:27:21 +0600

    очередной коммит

--- app/views/home/checker.blade.php
+++ app/views/home/checker.blade.php
@@ -122,7 +122,7 @@
     </form>
 
     <div>
-      <iframe sandbox src="{{ $url }}" id="load-container" style="width: 100%; height: 600px; border: 1px solid #CECECE;" onload="frameLoaded()"></iframe>
+      <iframe sandbox src="{{ $url }}" name="load_container" id="load-container" style="width: 100%; height: 600px; border: 1px solid #CECECE;" onload="frameLoaded()"></iframe>
     </div> 
     
   </div>
@@ -135,7 +135,7 @@
 function checkPhones(){
     
     console.log( $('#load-container').contents() );
-    console.log( document.getElementById("load-container").documentWindow );
+    console.log( document.getElementById("load-container").contentWindow );
     
     $('#check-phones-content').html('<i>выполнение операции...</i>');
     $.ajax({
