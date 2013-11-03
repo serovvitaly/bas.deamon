f509609b5252a4ca4eaa7ea21c2d4a6298513955
Vitaly Serov <serovvitaly@gmail.com>
2013-11-03 16:01:48 +0600

    очередной коммит

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -238,9 +238,7 @@ class HomeController extends BaseController {
                 
                 $data = json_decode(Site::find($uid)->data);
                 
-                print_r($data);
-                
-                if (is_array($data) AND count($data) > 0) {
+                if ($data AND count($data) > 0) {
                     array_walk_recursive($data, function($item, $key){
                         print_r($item);
                     });
