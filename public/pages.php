6025a9af1dda93d496a33c921398adf4b92ab138
Vitaly Serov <serovvitaly@gmail.com>
2013-12-15 19:03:25 +0400

    очередной коммит

--- public/pages.php
+++ public/pages.php
@@ -5,5 +5,5 @@ $db = new mysqli('localhost', 'root', 'Sy9YGKbG', 'test');
 $res = $db->query('SELECT * FROM `final_sites_list` WHERE `status` >= 2 ORDER BY `domain_created` DESC LIMIT 50');
 
 while ($row = $res->fetch_assoc()) {
-    echo "<p>{$row->url}</p>";
+    echo "<p>{$row['url']}</p>\n";
 }
