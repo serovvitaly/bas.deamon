cfa982f1e77bc61b98fb8c4cd95337e0e8608ffc
Vitaly Serov <serovvitaly@gmail.com>
2013-12-19 20:17:32 +0400

    коммит

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -281,6 +281,8 @@ class HomeController extends BaseController {
                             
                             $content = strip_tags($content);
                             preg_match_all($pattern, $content, $matches2, PREG_SET_ORDER);
+                            
+                            $out['matches'][] = $matches2;
                                                         
                             if (isset($matches2[0]) AND count($matches2[0]) > 0) {
                                 foreach ($matches2[0] AS $mm) {
